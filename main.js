4e9da858a20c18c89fc8dba02e880fa771510053
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-28 15:54:42 +0100

    add select

--- main.js
+++ main.js
@@ -8,26 +8,29 @@ console.log(btn, container);
 
 const cellNumber = 100;
 
-const markup = `<div class="box"></div>`;
+const markup = `<div class="box"></div>`; // serve a creare i div con classe box in html
 
 btn.addEventListener("click", play)
 
+/**
+ * Al click del bottone genera la griglia di gioco
+ */
 function play (){
  for (let index = 0; index < cellNumber; index++) {
-    container.insertAdjacentHTML("beforeend", markup)
-    container.style.backgroundColor = "red"
+    container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
+    container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
 }
 
 let myArray = []
 
-for (let index = 0; myArray.length < 100; index++) {
+for (let index = 0; myArray.length < 100; index++) {// serve a creare 100 numeri consecutivi
     const element = myArray[index];
-    myArray.push(index);
+    myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
 }
 
 console.log(myArray);
 
-const box = document.getElementsByClassName("box")
+const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
 console.log(box);
 
 for (let index = 0; index < box.length; index++) {
