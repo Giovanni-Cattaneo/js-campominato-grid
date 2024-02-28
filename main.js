e22b5d4a79326d90d4f285bd0b82601f0f489aec
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-28 15:41:09 +0100

    add log of number when click

--- main.js
+++ main.js
@@ -15,13 +15,24 @@ for (let index = 0; index < cellNumber; index++) {
     
 }
 
+let myArray = []
+
+for (let index = 0; myArray.length < 100; index++) {
+    const element = myArray[index];
+    myArray.push(index);
+}
+
+console.log(myArray);
+
 const box = document.getElementsByClassName("box")
 console.log(box);
 
 for (let index = 0; index < box.length; index++) {
     const element = box[index];
+ 
     console.log(element);
     element.addEventListener("click", function(){
         element.classList.toggle("blue")
+        console.log(myArray[index]);
     })
 }
