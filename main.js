31740d5da3462a75fd3d7dc75c13bdad582dd482
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-28 15:22:05 +0100

    add griglia

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 console.log("hello world");
 
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
 
 console.log(btn, container);
+
+const cellNumber = 100;
+
+const markup = `<div class="box"></div>`;
+
+for (let index = 0; index < cellNumber; index++) {
+    container.insertAdjacentHTML("beforeend", markup)
+    
+}
