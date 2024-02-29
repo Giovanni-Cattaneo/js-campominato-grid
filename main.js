2a59981ba8174b5baf1fd411bb7a3ef102aabd35
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-29 09:37:26 +0100

    better indentation

--- main.js
+++ main.js
@@ -19,70 +19,70 @@ btn.addEventListener("click", play)
 /**
  * Al click del bottone genera la griglia di gioco
  */
-function play (){
-    container.innerHTML =""
+function play() {
+    container.innerHTML = ""
     if (options.value === "standard") {
         cellNumber = 100;
-       for (let index = 0; index < cellNumber; index++) {
-    container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
-    container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
-}
+        for (let index = 0; index < cellNumber; index++) {
+            container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
+            container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
+        }
 
-let myArray = []
+        let myArray = []
 
-for (let index = 0; myArray.length < 100; index++) {// serve a creare 100 numeri consecutivi
-    const element = myArray[index];
-    myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
-}
+        for (let index = 0; myArray.length < 100; index++) {// serve a creare 100 numeri consecutivi
+            const element = myArray[index];
+            myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
+        }
 
-console.log(myArray);
+        console.log(myArray);
 
-const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
-console.log(box);
+        const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
+        console.log(box);
 
-for (let index = 0; index < box.length; index++) {
-    const element = box[index];
- 
-    console.log(element);
-    element.addEventListener("click", function(){
-        element.classList.toggle("blue")
-        console.log(myArray[index]);
-        
-    })
-}   
- 
-    } else if(options.value === "complesso") {
+        for (let index = 0; index < box.length; index++) {
+            const element = box[index];
+
+            console.log(element);
+            element.addEventListener("click", function () {
+                element.classList.toggle("blue")
+                console.log(myArray[index]);
+
+            })
+        }
+
+    } else if (options.value === "complesso") {
         cellNumber = 81;
         container.style.width = "900px"
         container.style.height = "900px"
         for (let index = 0; index < cellNumber; index++) {
             container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
             container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
-            
+
         }
-        
+
         let myArray = []
-        
+
         for (let index = 0; myArray.length < 100; index++) {// serve a creare 100 numeri consecutivi
             const element = myArray[index];
             myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
         }
-        
+
         console.log(myArray);
-        
+
         const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
         console.log(box);
-        
+
         for (let index = 0; index < box.length; index++) {
             const element = box[index];
-         
+
             console.log(element);
-            element.addEventListener("click", function(){
+            element.addEventListener("click", function () {
                 element.classList.toggle("blue")
                 console.log(myArray[index]);
             })
-        }   
-    }else if( options.value === "impossibile"){
+        }
+    } else if (options.value === "impossibile") {
         cellNumber = 49;
         container.style.width = "700px"
         container.style.height = "700px"
@@ -90,29 +90,29 @@ for (let index = 0; index < box.length; index++) {
             container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
             container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
         }
-        
+
         let myArray = []
-        
+
         for (let index = 0; myArray.length < 100; index++) {// serve a creare 100 numeri consecutivi
             const element = myArray[index];
             myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
         }
-        
+
         console.log(myArray);
-        
+
         const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
         console.log(box);
-        
+
         for (let index = 0; index < box.length; index++) {
             const element = box[index];
-         
+
             console.log(element);
-            element.addEventListener("click", function(){
+            element.addEventListener("click", function () {
                 element.classList.toggle("blue")
                 console.log(myArray[index]);
             })
-        }   
-        
+        }
+
     }
- 
+
 }
