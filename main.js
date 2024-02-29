e040ca259d551e21e7ad0524541c07176d46eb18
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-29 15:23:12 +0100

    edit main js if's

--- main.js
+++ main.js
@@ -78,71 +78,109 @@ function play() {
         }
 
     } else if (options.value === "complesso") {
-        cellNumber = 82;
+        cellNumber = 81;
         container.style.width = "900px"
         container.style.height = "900px"
-        for (let index = 1; index < cellNumber; index++) {
+        for (let index = 0; index < cellNumber; index++) {
             container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
-            container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
-
+            container.style.backgroundColor = "blue" // diamo uno stile al bg solo al click dell'avvio del gioco
         }
 
         let myArray = []
 
-        for (let index = 1; myArray.length < cellNumber; index++) {// serve a creare 100 numeri consecutivi
+        let skullArray = []
+
+
+
+        for (let index = 0; myArray.length < cellNumber; index++) {// serve a creare 100 numeri consecutivi
             const element = myArray[index];
             myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
         }
 
-        console.log(myArray);
+        for (let index = 0; skullArray.length < 17; index++) {
+            const randomNumber = getRndInteger(1, 100);
+            if (!skullArray.includes(randomNumber)) { // Verifica se il numero casuale non è già presente nell'array
+                skullArray.push(randomNumber);
+            }
+        }
+
+        console.log(myArray, skullArray);
 
         const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
-        console.log(box);
 
         for (let index = 0; index < box.length; index++) {
             const element = box[index];
 
-            console.log(element);
-            element.addEventListener("click", function () {
-                element.classList.toggle("blue")
-                console.log(myArray[index]);
-                element.innerHTML = myArray[index]
+            element.addEventListener("click", function clickBox() {
+                
+                const sharedNumbers = skullArray.indexOf(myArray[index]); // indexof() ci permette di paragonare gli indici degli array per verificare se combaciano in tutto o in parte, il risultato è o -1 se non combaciano oppure la posizione dell'elemento nell'array
+                
+                    if (sharedNumbers !== -1) { // !== controlla se il avlore dell'elemento non corrisponde a -1, se non corrisponde allora l'elemento è condiviso senno non lo è ci aspettiamo 84 -1 in console
+                        const boxElement = box[index]; // costante creata per recuperare index di box dalla dom
+                        element.classList.add("red")
+                        boxElement.innerHTML = skull;
+                        console.log("You lose");
+                        element.removeEventListener("click", clickBox); // dovrebbe rimuovere l'elemento click ma non lo fa indagare meglio dopo
+                        
+                    } else {
+                        element.innerHTML = heart
+                        element.classList.add("green")
+                    }
             })
         }
     } else if (options.value === "impossibile") {
-        cellNumber = 50;
+        cellNumber = 49;
         container.style.width = "700px"
         container.style.height = "700px"
-        for (let index = 1; index < cellNumber; index++) {
+        for (let index = 0; index < cellNumber; index++) {
             container.insertAdjacentHTML("beforeend", markup)// inseriamo il markup
-            container.style.backgroundColor = "red" // diamo uno stile al bg solo al click dell'avvio del gioco
+            container.style.backgroundColor = "blue" // diamo uno stile al bg solo al click dell'avvio del gioco
         }
 
         let myArray = []
 
-        for (let index = 1; myArray.length < cellNumber; index++) {// serve a creare 100 numeri consecutivi
+        let skullArray = []
+
+
+
+        for (let index = 0; myArray.length < cellNumber; index++) {// serve a creare 100 numeri consecutivi
             const element = myArray[index];
             myArray.push(index);// pusha nell'array vuota ogni numero man mano che viene generato
         }
 
-        console.log(myArray);
+        for (let index = 0; skullArray.length < 17; index++) {
+            const randomNumber = getRndInteger(1, 100);
+            if (!skullArray.includes(randomNumber)) { // Verifica se il numero casuale non è già presente nell'array
+                skullArray.push(randomNumber);
+            }
+        }
+
+        console.log(myArray, skullArray);
 
         const box = document.getElementsByClassName("box")// ci permette di cerare un array di box accessibili alle funzioni, ora sono nodi della dom
-        console.log(box);
 
         for (let index = 0; index < box.length; index++) {
             const element = box[index];
 
-            console.log(element);
-            element.addEventListener("click", function () {
-                element.classList.toggle("blue")
-                console.log(myArray[index]);
-                element.innerHTML = myArray[index]
+            element.addEventListener("click", function clickBox() {
+                
+                const sharedNumbers = skullArray.indexOf(myArray[index]); // indexof() ci permette di paragonare gli indici degli array per verificare se combaciano in tutto o in parte, il risultato è o -1 se non combaciano oppure la posizione dell'elemento nell'array
+                
+                    if (sharedNumbers !== -1) { // !== controlla se il avlore dell'elemento non corrisponde a -1, se non corrisponde allora l'elemento è condiviso senno non lo è ci aspettiamo 84 -1 in console
+                        const boxElement = box[index]; // costante creata per recuperare index di box dalla dom
+                        element.classList.add("red")
+                        boxElement.innerHTML = skull;
+                        console.log("You lose");
+                        element.removeEventListener("click", clickBox); // dovrebbe rimuovere l'elemento click ma non lo fa indagare meglio dopo
+                        
+                    } else {
+                        element.innerHTML = heart
+                        element.classList.add("green")
+                    }
             })
         }
 
-    }
-
+}
 }
 
 
@@ -176,4 +214,3 @@ function generateGrid(cellNumber){
 //         element.innerHTML = myArray[index];
 //         element.classList.add("green");
 //     }
-// }
